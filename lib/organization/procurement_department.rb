e0bf84f0f52c48bb205ea4ab72e7434d0da4668e
vachanda <vachan.da@urbanladder.com>
2015-06-11 10:33:42 +0530

    Added specs and inventory for procurement

--- lib/organization/procurement_department.rb
+++ lib/organization/procurement_department.rb
@@ -1,8 +1,9 @@
 #Represents an operational division in an organization 
 class Organization::ProcurementDepartment
-	attr_reader :cash
-	def initialize(name, funds)
+	attr_reader :cash, :inventory_count
+	def initialize(name, funds, inventory_count)
 		@name = name
 		@cash = funds
+		@inventory_count	= inventory_count
 	end
 end
