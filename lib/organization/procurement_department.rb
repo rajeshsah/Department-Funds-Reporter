d1d0c018bddc67397e5cd50eb1b5700032969b61
vachanda <vachan.da@urbanladder.com>
2015-06-11 10:52:26 +0530

    Added inventory method

--- lib/organization/procurement_department.rb
+++ lib/organization/procurement_department.rb
@@ -1,9 +1,9 @@
 #Represents an operational division in an organization 
 class Organization::ProcurementDepartment
-	attr_reader :cash, :inventory_count
+	attr_reader :cash, :inventory
 	def initialize(name, funds, inventory_count)
 		@name = name
 		@cash = funds
-		@inventory_count	= inventory_count
+		@inventory	= inventory_count
 	end
 end
